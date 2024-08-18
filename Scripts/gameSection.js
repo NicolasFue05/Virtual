afb9fec39cc51afd8ef89ba0b1f1a84c8da05421
NicolasFue05 <nicolasfue05@gmail.com>
2024-08-18 06:25:00 -0500

    :feat: get data function added

---
+++ Scripts/gameSection.js
@@ -0,0 +1,52 @@
+const gameBigTitle = document.querySelector('.header-name h1');
+const displayImage = document.querySelector('.show-image-container img')
+const imageButtons = document.getElementsByClassName('image-change-btn')
+
+// Get the games data (it has to be opened on a local server like the Live Server Extension)
+async function getData() {
+    const url = "../Scripts/games.json"
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            console.error(`Ha ocurrido un Error | ${response.status}`)
+        } else {
+            const data = await response.json();
+            return data
+        }
+    }
+    catch (err) {
+        console.error(err)
+        return null
+    }
+}
+
+
+
+// Show the data into the page
+function showGameData(gameName) {
+    try {
+        getData().then(response => {
+            console.log(response.Games)
+
+            // Store the buttons into an array
+
+            // Loops through the json until the game name matches
+            for (let i = 0; i < response.Games.length; i++) {
+                //if the name match change it in all the nodes
+                if (gameName == response.Games[i].name) {
+                    gameBigTitle.textContent = response.Games[i].name; // Set the Big Name
+                    displayImage.setAttribute('src', `${response.Games[i].imageFrontpage}`) // set the display image
+                    // Set the images into the buttons
+                    for (let j = 0; j > 4; j++){
+                        
+                    }
+                    break
+                }
+            }
+
+        })
+    }
+    catch (err) {
+        console.error(err)
+    }
+}
