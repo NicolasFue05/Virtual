c1643e131ebd780cdb05737643cade969fe3f8ed
NicolasFue05 <nicolasfue05@gmail.com>
2024-08-19 19:39:55 -0500

    :fix: some bugs fixed

--- Scripts/app.js
+++ Scripts/app.js
@@ -2,13 +2,26 @@ const menuButton = document.getElementById('menubtn');
 const menu = document.getElementById('menu');
 const menuList = document.getElementById('menu-list');
 
-menuButton.addEventListener('click', function() {
+
+menuButton.addEventListener('click', function () {
     if (menu.style.display === 'none' || menu.style.display === '') {
-        menu.style.display = 'block';  // Mostrar el menú
-        menu.classList.add('menu-animation');  // Añadir animación
-        document.body.display = 'none'
+        // Mostrar el menú
+        menu.style.display = 'block';  
+        menu.classList.add('menu-animation');
+        menuList.classList.add('menu-animation');  
     } else {
-        menu.classList.add('slide-back')        
+        // Añadir clases de animación de regreso
+        menu.classList.add('slide-back');
+        menuList.classList.add('slide-back');
         menu.classList.remove('menu-animation');
+        menuList.classList.remove('menu-animation');
+
+        // Esperar a que la animación termine antes de ocultar el menú
+        menu.addEventListener('animationend', () => {
+            // Limpiar clases
+            menu.style.display = 'none';
+            menu.classList.remove('slide-back');  
+            menuList.classList.remove('slide-back');
+        }, { once: true });  
     }
 });
